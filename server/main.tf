6aad0de7cc6562fc45c9a17fd1c479102859247e
Jviiith <jonathanokon98@gmail.com>
2024-06-04 01:31:35 +0100

    updated

--- server/main.tf
+++ server/main.tf
@@ -1,10 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
+
 
 # 1. Create a VPC
 resource "aws_vpc" "vpc" {
